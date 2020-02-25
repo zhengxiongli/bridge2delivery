a17e6ebf0fbb2133fdc772812b88fddbc9f8659e
zhengxiongli <xlzheng@thoughtworks.com>
2020-02-25 17:10:37 +0800

    重构thymeleaf后端渲染工具类

--- src/main/java/com/thoughtworks/bridge2delivery/utils/ThymeleafUtils.java
+++ src/main/java/com/thoughtworks/bridge2delivery/utils/ThymeleafUtils.java
@@ -2,20 +2,36 @@ package com.thoughtworks.bridge2delivery.utils;
 
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
 
 import java.util.Map;
 
 @Component
 public class ThymeleafUtils {
     private final SpringTemplateEngine stringTemplateEngine;
-    private ThymeleafUtils(final SpringTemplateEngine stringTemplateEngine) {
-        this.stringTemplateEngine = stringTemplateEngine;
+    private ThymeleafUtils() {
+        this.stringTemplateEngine = getStringTemplateEngine();
     }
 
     public String renderTemplate(String template, Map<String, Object> variables) {
         Context ctx = new Context();
         ctx.setVariables(variables);
         return stringTemplateEngine.process(template, ctx);
+    }
+
+    private static SpringTemplateEngine getStringTemplateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        IDialect iDialect = new SpringStandardDialect();
+        springTemplateEngine.setDialect(iDialect);
+
+        StringTemplateResolver stringTemplateResolver = new StringTemplateResolver();
+        stringTemplateResolver.setCacheable(true);
+        stringTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        springTemplateEngine.setTemplateResolver(stringTemplateResolver);
+        return springTemplateEngine;
     }
 }
