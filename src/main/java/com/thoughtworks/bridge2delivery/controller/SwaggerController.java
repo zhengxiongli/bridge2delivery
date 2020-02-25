a70709e7e90066e37f4e61951d8bcc51ed525a20
zhengxiongli <xlzheng@thoughtworks.com>
2020-02-25 09:44:24 +0800

    添加swagger

--- src/main/java/com/thoughtworks/bridge2delivery/controller/SwaggerController.java
+++ src/main/java/com/thoughtworks/bridge2delivery/controller/SwaggerController.java
@@ -3,23 +3,31 @@ package com.thoughtworks.bridge2delivery.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.http.HTTPException;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/swagger")
-public class swaggerController {
-
+public class SwaggerController {
+    private static final int BUFF_SIZE = 1024;
     @RequestMapping(value = "upload", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public String upload(@RequestParam("swaggerFile")  MultipartFile file, HttpServletResponse response) throws FileNotFoundException {
         System.out.println(file);
-        if(file.isEmpty()) {
+        if (file.isEmpty()) {
             return "文件不能为空";
         }
         this.downloadWord(response);
@@ -33,10 +41,10 @@ public class swaggerController {
         response.setContentType("application/octet-stream;charset=utf-8");
         response.setCharacterEncoding("utf-8");
         response.setContentLength((int) file.length());
-        response.setHeader("Content-Disposition", "attachment;filename=" + file.getName() );
+        response.setHeader("Content-Disposition", "attachment;filename=" + file.getName());
 
-        try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));) {
-            byte[] buff = new byte[1024];
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));) {
+            byte[] buff = new byte[BUFF_SIZE];
             OutputStream os  = response.getOutputStream();
             int i = 0;
             while ((i = bis.read(buff)) != -1) {
