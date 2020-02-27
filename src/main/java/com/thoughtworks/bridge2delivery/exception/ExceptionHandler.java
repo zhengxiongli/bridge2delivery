4a5d0a4e9ef01a1f8a7b7f90b686ae4cf37f876b
zhengxiongli <xlzheng@thoughtworks.com>
2020-02-27 10:18:06 +0800

    添加统一错误处理

--- src/main/java/com/thoughtworks/bridge2delivery/exception/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/bridge2delivery/exception/ExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.bridge2delivery.exception;
 
 import com.thoughtworks.bridge2delivery.dto.ApiResponse;
+import com.thoughtworks.bridge2delivery.swagger.model.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.text.html.parser.Entity;
 
 @Slf4j
 @ControllerAdvice
@@ -13,8 +17,19 @@ public class ExceptionHandler {
 
     @ResponseBody
     @org.springframework.web.bind.annotation.ExceptionHandler(value = CustomException.class)
+    @Order(1)
     public ResponseEntity<ApiResponse> handleCustomException(CustomException exception) {
+        log.error("custom error", exception);
         return new ResponseEntity<ApiResponse>(ApiResponse.error(exception.getMessage()),
+                HttpStatus.FORBIDDEN);
+    }
+
+    @ResponseBody
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
+    @Order(2)
+    public ResponseEntity<ApiResponse> handleException(Exception e) {
+        log.error("exception", e);
+        return new ResponseEntity<ApiResponse>(ApiResponse.error(e.getMessage()),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
