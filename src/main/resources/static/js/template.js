8e864d802cdc3f5a7657c1eca4abfee23c107490
syxiao <syxiao@thoughtworks.com>
2020-03-03 17:56:33 +0800

    init default template

--- src/main/resources/static/js/template.js
+++ src/main/resources/static/js/template.js
@@ -4,17 +4,40 @@ import EDITOR_CONFIG from './editor.config.js'
 const ue = UE.getEditor('editor', EDITOR_CONFIG);
 
 function addGenerateButton() {
-    const wrapper = document.createElement('div')
-    wrapper.className = 'template-generate-btn-wrapper'
-    const button = document.createElement('button')
-    button.innerText = '生成'
-    button.className = 'template-generate-btn'
-    wrapper.append(button)
+    const wrapper = document.createElement('div');
+    wrapper.className = 'template-generate-btn-wrapper';
+    const button = document.createElement('button');
+    button.innerText = '生成';
+    button.className = 'template-generate-btn';
+    button.addEventListener('click', generateTemplate);
+    wrapper.append(button);
     $('.edui-editor-toolbarboxinner').append(wrapper)
 }
 
+function generateTemplate() {
+    console.log(ue.getContent())
+}
 
-ue.addListener('ready', (editor) => {
-    addGenerateButton()
-    ue.execCommand('drafts');
+function initDefaultTemplate() {
+    fetch('/template/swagger.html', {
+        method: 'GET'
+    })
+        .then((res) => res.blob())
+        .then((blob) => {
+            const reader = new FileReader();
+            reader.readAsText(blob, 'UTF-8');
+            reader.onloadend = () => {
+                ue.setContent(reader.result)
+            }
+        });
+}
+
+ue.addListener('ready', () => {
+    const drafts = ue.execCommand('getlocaldata');
+    if (drafts) {
+        ue.execCommand('drafts');
+    } else {
+        initDefaultTemplate();
+    }
+    addGenerateButton();
 });
