2ecd29ba3313f92d6957ab67d431df44fd3a1cc1
zhengxiongli <xlzheng@thoughtworks.com>
2020-03-11 18:33:11 +0800

    修改下划线不能取消问题

--- src/main/resources/static/js/ueditor/ueditor.all.js
+++ src/main/resources/static/js/ueditor/ueditor.all.js
@@ -11875,7 +11875,7 @@ UE.plugins['font'] = function () {
                         while (tmpNode && !domUtils.isBlockElm(tmpNode) && !domUtils.isBody(tmpNode)) {
                             if (tmpNode.nodeType == 1) {
                                 value = domUtils.getComputedStyle(tmpNode, style);
-                                if (value != 'none') {
+                                if (value.indexOf('none') < 0) {
                                     return value;
                                 }
                             }
